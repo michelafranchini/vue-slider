4b9b70141a292fdcbfd9751a80d5dbae71bf0c49
michelafranchini <michela.franchini.91@gmail.com>
2021-05-18 14:46:12 +0200

    Struttura base

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var app = new Vue(
+    {
+        el: "#root", 
+        data: {
+            images: [
+                "https://giochipertutti.org/wp-content/uploads/2020/05/Aggiornamento-CSGO-05042020-v1100-rilasciato-miglioramenti-e-correzioni.jpg", 
+                "https://paladins.it/wp-content/uploads/2021/03/R6_Siege.jpeg", 
+                "https://www.outplayed.it/wp-content/uploads/2020/10/Valorant-3.jpg", 
+                "https://rhw.coop/sites/default/files/hearthstone_fxgu.jpg", 
+                "https://xboxplay.games/uploadStream/7818.jpg", 
+                "https://www.gameower.it/wp-content/uploads/2018/04/maxresdefault.jpg"
+            ],
+            indexImages: 0 
+        }, 
+        methods:  {
+            prevImage: function() {
+                this.indexImages--; 
+                if (this.indexImages < 0) {
+                    this.indexImages = this.images.length - 1; 
+                }
+            }, 
+            nextImage: function() {
+                this.indexImages++;
+                if (this.indexImages == this.images.length) {
+                    this.indexImages = 0; 
+                }
+            }
+        }
+    }
+)
